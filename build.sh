1d23966ef88f5321130508fa7ce91566a74cbac6
mulab <mulab@googlegroups.com>
2010-11-28 08:02:07 +0000

    [mU] 修改build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-if [ ! -d build ];then mkdir build && cp mU.ini build/;fi
+if [ ! -d build ];then mkdir build;fi
 cd build
 cmake -G"Eclipse CDT4 - Unix Makefiles" -DECLIPSE_CDT4_GENERATE_SOURCE_PROJECT=TRUE ..
 cp .project ..
 cp .cproject ..
 make
+cp mU ..
+cp *.dll ../Package/
+cp *.so ../Package/
