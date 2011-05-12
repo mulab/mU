83b871a448ad521147e0f03e0249353f2a404647
Hao Song <baritono.tux@gmail.com>
2011-05-12 09:19:48 +0000

    [mU] 初步编写bootstrap.bat大致流程，帮助入门开发者在Windows系统上安装所需软件和库，并指导入门操作

---
+++ bootstrap.bat
@@ -0,0 +1,11 @@
+:: download and install tortoise-svn
+:: if visual c++
+::     download and install cmake
+::     download gmp.dll
+::     download and compile boost
+::     generate visual studio projects with cmake
+:: else
+::     download mingw image, unpack, set PATH environment variable
+::     download and install cmake
+::     download and compile boost
+::     generate makefiles with cmake
